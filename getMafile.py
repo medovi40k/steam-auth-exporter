884b845b5f16c6898d0b5e26da6b1b382dc41a09
medovi40k <74722105+medovi40k@users.noreply.github.com>
2022-06-29 16:12:17 +0300

    Version 1.0.1

--- getMafile.py
+++ getMafile.py
@@ -3,26 +3,31 @@
 import os
 import json
 import xml.etree.ElementTree as ET
+import time
 
 def getdeviceid():
+	print("Getting steam.uuid.xml")
 	b=os.popen("adb root & adb shell cat /data/data/com.valvesoftware.android.steam.community/shared_prefs/steam.uuid.xml").read()
 	myroot = ET.fromstring(b)
 	deviceid = [e.text for e in myroot.findall('.//string')]
 	return deviceid[0]
 if __name__ == '__main__':
+	print("Getting auth from your phone")
 	a=os.popen('adb root & adb shell cat /data/data/com.valvesoftware.android.steam.community/files/*')
-
+	time.sleep(0.1)
 	data1=json.load(a)
 	steamid=data1["steamid"]
+	print("Creating file")
 	f1 = open("replacement.maFile", "r")
 	f2 = open(steamid+".maFile", "w")
-
+	time.sleep(0.1)
 	f2.write(f1.read())
 	f2.close()
 	f1.close()
-
 	with open(steamid+".maFile", "r") as file1:
 		file=file1.read()
+		print("Doing replaces")
+		time.sleep(0.1)
 		file=file.replace("REPLACEMENTN1", data1["shared_secret"], 1)
 		file=file.replace("REPLACEMENTN2", data1["serial_number"], 1)
 		file=file.replace("REPLACEMENTN3", data1["revocation_code"], 1)
@@ -34,7 +39,9 @@ if __name__ == '__main__':
 		file=file.replace("REPLACEMENTN9", data1["secret_1"], 1)
 		file=file.replace("REPLACEMENTN10", getdeviceid(), 1)
 		file=file.replace("REPLACEMENTN11", data1["steamid"], 1)
-
 		with open(steamid+".maFile", "w") as file2:
+			print("Making your maFile")
+			time.sleep(0.1)
 			file2.write(file)
+	input("Press enter to exit: ")
 
